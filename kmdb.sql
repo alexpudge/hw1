bf9edb60c71a94fe2f193008c470f287f0bd54be
alexpudge <alex.feldman@kellogg.northwestern.edu>
2022-01-24 17:19:05 +0000

    making progress

--- kmdb.sql
+++ kmdb.sql
@@ -67,17 +67,42 @@
 .headers off
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
-DROP TABLE IF EXISTS movies;
-DROP TABLE IF EXISTS directors;
-DROP TABLE IF EXISTS actors;
 
+DROP TABLE IF EXISTS directors;
+DROP TABLE IF EXISTS movies;
+DROP TABLE IF EXISTS roles;
 
 -- Create new tables, according to your domain model
--- TODO!
+
+CREATE TABLE directors (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  full_name TEXT
+);
+
+CREATE TABLE movies (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  title TEXT,
+  year_released TEXT,
+  mpaa_rating TEXT,
+  directors_id INTEGER
+);
+
+CREATE TABLE roles (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    movies_id INTEGER,
+    actor_name TEXT,
+    character_name TEXT
+);
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
--- TODO!
+
+INSERT INTO directors (full_name)
+VALUES ("Christopher Nolan");
+
+INSERT INTO movies (title, year_released, mpaa_rating, directors_id)
+VALUES ("Batman Begins", 2005, "PG-13", 1);
+
 
 -- Prints a header for the movies output
 .print "Movies"
